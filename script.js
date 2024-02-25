88ecb1c138f15d4e3d07a1c90fd971de123c6b96
Vinicius Lima <viniuciusyl@hotmail.com>
2024-02-25 05:34:21 -0300

    teste5

---
+++ script.js
@@ -0,0 +1,10 @@
+function trocarImagem() {
+    var imagem = document.querySelector('.image img');
+    imagem.src = './image/produtos/maquiagem/AdversaMáscaraDeCíliosVegana2.png';
+}
+
+function restaurarImagem() {
+    var imagem = document.querySelector('.image img');
+    imagem.src = './image/produtos/maquiagem/AdversaMáscaraDeCíliosVegana1.png';
+}
+
