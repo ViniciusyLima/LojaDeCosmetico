49e2bf49b61e934062b3910b59b1b31dc2c5204e
Vinicius Lima <viniuciusyl@hotmail.com>
2024-02-27 16:43:37 -0300

    v1

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-// Verifica se o navegador é Safari
 function isSafari() {
     return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 }
 
-// Função para carregar o arquivo CSS específico para o Safari e desativar o arquivo style.css
-function loadSafariStylesheet() {
-    var defaultStyle = document.getElementById('defaultStyle');
-    defaultStyle.disabled = true; // Desativa o arquivo style.css
+// Função para modificar os estilos dos elementos para o Safari
+function modifyStylesForSafari() {
 
-    var safariStyle = document.getElementById('safariStyle');
-    safariStyle.disabled = false; // Ativa o arquivo safari-style.css
+
 }
 
-// Carrega o arquivo CSS específico para o Safari e desativa o arquivo style.css se necessário
+// Modifica os estilos dos elementos para o Safari, se necessário
 if (isSafari()) {
-    loadSafariStylesheet();
-    //lert("ATENÇÃO CORNO(A)" + '\n\n' + "Se você é modinha de iPhone e é corno(a), clique em fechar!");
-} else {
-    //alert("Este cliente não está usando o Safari");
+    modifyStylesForSafari();
 }
-
