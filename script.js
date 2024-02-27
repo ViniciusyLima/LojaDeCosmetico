cbdfcf6b226fa825664ee6c328c2014c02b56657
Vinicius Lima <viniuciusyl@hotmail.com>
2024-02-27 14:04:05 -0300

    Versão para navegador safari

---
+++ script.js
@@ -0,0 +1,18 @@
+// Verifica se o navegador é Safari
+function isSafari() {
+    return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+}
+
+// Função para carregar o arquivo CSS específico para o Safari e desativar o arquivo style.css
+function loadSafariStylesheet() {
+    var defaultStyle = document.getElementById('defaultStyle');
+    defaultStyle.disabled = true; // Desativa o arquivo style.css
+
+    var safariStyle = document.getElementById('safariStyle');
+    safariStyle.disabled = false; // Ativa o arquivo safari-style.css
+}
+
+// Carrega o arquivo CSS específico para o Safari e desativa o arquivo style.css se necessário
+if (isSafari()) {
+    loadSafariStylesheet();
+}
